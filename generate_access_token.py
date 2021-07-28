5ea5efe6847bb89171d40cb9ca2f10c3a0bcb316
kedareswara swamy <48353702+swamy086@users.noreply.github.com>
2021-07-28 09:59:22 +0530

    Create generate_access_token.py

---
+++ generate_access_token.py
@@ -0,0 +1,3 @@
+from alice_blue import *
+access_token = AliceBlue.login_and_get_access_token(username='193734', password='PKswamy@15697', twoFA='a',  api_secret='XELKQq1kg8PYxY4fSJ8j2S3NtVh1AZSXAms1RV91XhGcQiloAvBFblsex3n41jV6', redirect_url='https://ant.aliceblueonline.com/plugin/callback', app_id='SePv1JYhfu')
+print(access_token)
