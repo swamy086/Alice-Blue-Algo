abbdb649b79e2c5dd41f6b3706655f6d519b1f72
kedareswara swamy <48353702+swamy086@users.noreply.github.com>
2021-07-28 10:44:00 +0530

    Update trade.py

--- trade.py
+++ trade.py
@@ -1,3 +1,3 @@
 from alice_blue import *
-access_token = AliceBlue.login_and_get_access_token(username='193734', password='PKswamy@15697', twoFA='a',  api_secret='tMojrYcoSmM5ZGIQcQjO9n008WJfO8f6MoBLaekP1cKnSV43c1AIOTWlr93tH4BS')
+access_token = AliceBlue.login_and_get_access_token(username='193734', password='AUG@micro123', twoFA='a',  api_secret='tMojrYcoSmM5ZGIQcQjO9n008WJfO8f6MoBLaekP1cKnSV43c1AIOTWlr93tH4BS')
 print(access_token)
